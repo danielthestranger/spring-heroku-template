d85e2a8e57f831f84221d7038c1c91e670d81c28
Peter <wrkpeter@gmail.com>
2019-01-25 05:39:58 +0100

    atari(admin) atariprovider added

--- src/main/java/com/greenfoxacademy/springherokutemplate/controller/AdminController.java
+++ src/main/java/com/greenfoxacademy/springherokutemplate/controller/AdminController.java
@@ -1,7 +1,9 @@
 package com.greenfoxacademy.springherokutemplate.controller;
 
+import com.greenfoxacademy.springherokutemplate.model.AtariProvider;
 import com.greenfoxacademy.springherokutemplate.model.Location;
 import com.greenfoxacademy.springherokutemplate.model.ServiceType;
+import com.greenfoxacademy.springherokutemplate.service.AtariProviderService;
 import com.greenfoxacademy.springherokutemplate.service.LocationService;
 import com.greenfoxacademy.springherokutemplate.service.ServiceTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +17,14 @@ public class AdminController {
 
   private ServiceTypeService serviceTypeService;
   private LocationService locationService;
+  private AtariProviderService atariProviderService;
 
   @Autowired
-  public AdminController(ServiceTypeService serviceTypeService, LocationService locationService) {
+  public AdminController(ServiceTypeService serviceTypeService, LocationService locationService,
+                      AtariProviderService atariProviderService) {
     this.serviceTypeService = serviceTypeService;
     this.locationService = locationService;
+    this.atariProviderService = atariProviderService;
   }
 
   @ModelAttribute
@@ -28,7 +33,8 @@ public class AdminController {
     model.addAttribute("ServiceTypes", serviceTypeService.findAll());
     model.addAttribute("newLocation", new Location());
     model.addAttribute("locations", locationService.getAllLocations());
-
+    model.addAttribute("atariproviders", atariProviderService.findAll());
+    model.addAttribute("newAtariProvider", new AtariProvider());
   }
 
 
@@ -79,6 +85,30 @@ public class AdminController {
                             @RequestParam(value = "newLocation", required = false) String newName) {
     try {
       locationService.findCreateOrUpdate(newName, id);
+    } catch (NullPointerException exception) {
+      System.out.println("not found" + exception);
+    }
+    return "redirect:/admin/";
+  }
+
+  @GetMapping("admin/{id}/deleteAtariProvider")
+  public String deleteAtariProvider(@PathVariable(value = "id") Long id) {
+    atariProviderService.deleteAtariProvider(id);
+    return "redirect:/admin";
+  }
+
+  @GetMapping("admin/{id}/editAtariProvider")
+  public String editAtariProvider(@PathVariable(value = "id") Long id, Model model) {
+    AtariProvider atariProvider = atariProviderService.findById(id);
+    model.addAttribute("newAtariProvider", atariProvider);
+    return "admin";
+  }
+
+  @PostMapping("admin/addAtariProvider")
+  public String addAtariProvider(@RequestParam(value = "providerid", required = false) Long id,
+                            @RequestParam(value = "newAtariProvider", required = false) String newName) {
+    try {
+      atariProviderService.findCreateOrUpdate(newName, id);
     } catch (NullPointerException exception) {
       System.out.println("not found" + exception);
     }
---
+++ src/main/java/com/greenfoxacademy/springherokutemplate/service/AtariProviderServiceImpl.java
@@ -0,0 +1,60 @@
+package com.greenfoxacademy.springherokutemplate.service;
+
+import com.greenfoxacademy.springherokutemplate.model.AtariProvider;
+import com.greenfoxacademy.springherokutemplate.repository.AtariProviderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AtariProviderServiceImpl implements AtariProviderService {
+
+  private AtariProviderRepository atariProviderRepository;
+
+
+  @Autowired
+  public AtariProviderServiceImpl(AtariProviderRepository atariProviderRepository) {
+    this.atariProviderRepository = atariProviderRepository;
+  }
+
+  @Override
+  public List<AtariProvider> findAll() {
+    return atariProviderRepository.findAll();
+  }
+
+  @Override
+  public void saveAtariProvider(AtariProvider atariProvider) {
+    atariProviderRepository.save(atariProvider);
+  }
+
+  @Override
+  public void deleteAtariProvider(Long id) {
+    atariProviderRepository.delete(atariProviderRepository.findById(id).orElse(null));
+  }
+
+  @Override
+  public AtariProvider findById(Long id) {
+    return atariProviderRepository.findById(id).orElse(null);
+  }
+
+  @Override
+  public AtariProvider findCreateOrUpdate(String newName, Long id) {
+    if (newName == null || newName.isEmpty()) {
+      throw new NullPointerException("Attribute name is null or empty");
+    }
+    if (id == null) {
+      AtariProvider savedAtariProvider = atariProviderRepository.findByDescription(newName);
+      if (savedAtariProvider == null) {
+        AtariProvider atariProvider = new AtariProvider(newName);
+        savedAtariProvider = atariProviderRepository.save(atariProvider);
+        return savedAtariProvider;
+      } else if (savedAtariProvider.getDescription().equals(newName)) {
+        return savedAtariProvider;
+      }
+    }
+    AtariProvider updateAttribute = atariProviderRepository.findById(id).orElse(null);
+    updateAttribute.setDescription(newName);
+    return atariProviderRepository.save(updateAttribute);
+  }
+}
